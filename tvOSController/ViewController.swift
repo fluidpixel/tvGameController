e71f69bedefcc52d1849997b70143760640fd455
Stuart Varrall <git@fpstudios.com>
2015-10-22 14:22:40 +0100

    (fix) bridging header reference + added button

--- tvOSController/ViewController.swift
+++ tvOSController/ViewController.swift
@@ -15,10 +15,14 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        remote.sendInfo(["launch":true])
+        
         
     }
 
+    @IBAction func sendAction(sender: AnyObject) {
+        remote.sendInfo(["launch":true])
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
